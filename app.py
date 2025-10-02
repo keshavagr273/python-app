287f74ef0a8122a858aac9f7e1010d906fadb79e
Keshav Agrawal <keshavagrawal273@gmail.com>
2025-10-02 17:13:01 +0530

    Intial

--- app.py
+++
@@ -1,2 +0,0 @@
-print("Hello, World!")
-print("This i a hnc")
---
+++ myproject/myproject/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+def welcome(request):
+    return HttpResponse("Welcome to our amazing Django application!")
+
+def dashboard(request):
+    return HttpResponse("This is your personalized dashboard with all the latest updates.")
+
+def about(request):
+    return HttpResponse("Learn more about our innovative platform and services.")    
